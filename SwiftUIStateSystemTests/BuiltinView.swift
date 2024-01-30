0d1cd6942a51514b04d1837597c500a5bcf73c94
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 17:17:58 +0530

    add `BuiltinNode`

---
+++ SwiftUIStateSystemTests/BuiltinView.swift
@@ -0,0 +1,12 @@
+//
+//  BuiltinView.swift
+//  SwiftUIStateSystemTests
+//
+//  Created by Karthik K Manoj on 30/01/24.
+//
+
+import Foundation
+
+protocol BuiltinView {
+    func _buildNodeTree(_ node: Node)
+}
