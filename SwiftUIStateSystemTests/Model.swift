0283d1d1458f5d878807bf020e22a50dff003ecb
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 16:06:47 +0530

    add `Model` object

---
+++ SwiftUIStateSystemTests/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  SwiftUIStateSystemTests
+//
+//  Created by Karthik K Manoj on 30/01/24.
+//
+
+import Foundation
+
+final class Model: ObservableObject {
+    @Published var counter: Int = 0
+}
