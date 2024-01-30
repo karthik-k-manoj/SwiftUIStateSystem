cad66d2c7390d39bb698e4e46a9f19227f0037db
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 13:30:36 +0530

    initial commit

---
+++ SwiftUIStateSystem/SwiftUIStateSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateSystemApp.swift
+//  SwiftUIStateSystem
+//
+//  Created by Karthik K Manoj on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
