c45f52c69f3790d308caf0715fc340462f61210e
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 16:03:45 +0530

    never conforms to view protocol

--- SwiftUIStateSystemTests/View.swift
+++ SwiftUIStateSystemTests/View.swift
@@ -11,3 +11,9 @@ protocol View {
     associatedtype Body: View
     var body: Body { get }
 }
+
+extension Never: View {
+    var body: Never {
+        fatalError("We should never reach this")
+    }
+}
