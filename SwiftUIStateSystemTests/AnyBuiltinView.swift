68ed28fab402bd12b69bad6e511f5c367c0009af
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 17:38:28 +0530

    add `AnyBuiltinView`

---
+++ SwiftUIStateSystemTests/AnyBuiltinView.swift
@@ -0,0 +1,20 @@
+//
+//  AnyBuiltinView.swift
+//  SwiftUIStateSystemTests
+//
+//  Created by Karthik K Manoj on 30/01/24.
+//
+
+import Foundation
+
+struct AnyBuiltinView: BuiltinView {
+    private var buildNodeTree: (Node) -> ()
+    
+    init<V: View>(_ view: V) {
+        self.buildNodeTree = view.buildNodeTree(_:)
+    }
+    
+    func _buildNodeTree(_ node: Node) {
+        buildNodeTree(node)
+    }
+}
