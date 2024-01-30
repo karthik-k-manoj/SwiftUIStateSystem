b9796f37e5d4500f82ed7c17413dc5e354eef70d
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 15:55:27 +0530

    move view file to test target

--- SwiftUIStateSystem/Button.swift
+++ SwiftUIStateSystem/Button.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Button: View {
+    var title: String
+    var action: () -> ()
+    
+    init(_ title: String, action: @escaping () -> ()) {
+        self.title = title
+        self.action = action
+    }
+    
+    var body: Never {
+        fatalError()
+    }
+}
