2a18f5f6ef0c5e2bb29a038c29d9e7c5cd1fa45d
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-01-30 17:20:27 +0530

    let view be implicitily unwrapped optional

--- SwiftUIStateSystemTests/Node.swift
+++ SwiftUIStateSystemTests/Node.swift
@@ -10,6 +10,7 @@ import Foundation
 final class Node {
     var children: [Node] = []
     var needsRebuild = true
+    var view: BuiltinView!
     
     func rebuildIfNeeded() {
         if needsRebuild {
